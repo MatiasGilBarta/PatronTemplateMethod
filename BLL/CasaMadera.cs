ce8cf985391704c1f95e6093d21cec2d424d9969
MatiasGilBarta <matiasgilbarta.dev@gmail.com>
2025-07-10 16:49:03 -0300

    Patron template method

---
+++ BLL/CasaMadera.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class CasaMadera : ConstruccionCasa
+    {
+        private readonly bool pintarRequerido;
+        private readonly bool construirSegundoPisoRequerido;
+        private readonly bool construirSotanoRequerido;
+
+        public CasaMadera(bool pintarRequerido, bool construirSegundoPisoRequerido, bool construirSotanoRequerido)
+        {
+            this.pintarRequerido = pintarRequerido;
+            this.construirSegundoPisoRequerido = construirSegundoPisoRequerido;
+            this.construirSotanoRequerido = construirSotanoRequerido;
+        }
+
+        //eso de arriba lo hacemos para que haya algun dato, para usarlo para decirnos si hay que usarlo o no, ya que son opcionales
+
+        protected override void ColocarAcabados()
+        {
+            Console.WriteLine("Colocando los acabados de una casa de madera\n");
+        }
+
+        protected override void ConstruirCimientos()
+        {
+            Console.WriteLine("Construyendo los cimientos de una casa de madera\n");
+        }
+
+        protected override void ConstruirPrimerPiso()
+        {
+            Console.WriteLine("Construyendo el primer piso de una casa de madera\n");
+        }
+
+        protected override void ConstruirTecho()
+        {
+            Console.WriteLine("Construyendo el techo de una casa de madera\n");
+        }
+
+        protected override void RealizarPlanos()
+        {
+            Console.WriteLine("Realizando los planos de una casa de madera\n");
+        }
+
+        protected override bool PintarRequerido()
+        {
+            return pintarRequerido;
+        }
+
+        protected override bool ConstruirSegundoPisoRequerido()
+        {
+            return construirSegundoPisoRequerido;
+        }
+
+        protected override bool ConstruirSotanoRequerido()
+        {
+            return construirSotanoRequerido;
+        }
+    }
+}
---
+++ PatronTemplateMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+
+namespace PatronTemplateMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool pintarRequerido1 = true;
+            bool construirSegundoPisoRequerido1 = false;
+            bool construirSotano1 = true;
+
+            ConstruccionCasa construccionCasa1 = new CasaLadrillo(pintarRequerido1, construirSegundoPisoRequerido1, construirSotano1);
+            construccionCasa1.ConstruirCasa();
+
+            bool pintarRequerido2 = true;
+            bool construirSegundoPisoRequerido2 = false;
+            bool construirSotano2 = true;
+
+            ConstruccionCasa construccionCasa2 = new CasaMadera(pintarRequerido2, construirSegundoPisoRequerido2, construirSotano2);
+            construccionCasa2.ConstruirCasa();
+            Console.ReadLine();
+        }
+    }
+}
